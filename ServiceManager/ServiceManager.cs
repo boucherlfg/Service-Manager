3d28654f80d475d9eea432ad3bdaf59cf4037775
Felix Boucher <boucherlfg@gmail.com>
2024-03-06 20:13:28 -0500

    add will add even if object already exists

--- ServiceManager/ServiceManager.cs
+++ ServiceManager/ServiceManager.cs
@@ -17,20 +17,11 @@ namespace ServiceManager
         /// <typeparam name="T">a class type</typeparam>
         /// <param name="builder">an override for generating the type</param>
         /// <returns>an object of type T</returns>
-        public T Get<T>() where T : class
-        {
-            return Get(GetDefaultConstructorObject<T>);
-        }
-
-        /// <summary>
-        /// returns the service instance corresponding to given class type
-        /// </summary>
-        /// <typeparam name="T">a class type</typeparam>
-        /// <param name="builder">an override for generating the type</param>
-        /// <returns>an object of type T</returns>
         /// <exception cref="Exception">returns an error if there is no service of type T found, and createIfInexistant is false</exception>
-        public T Get<T>(Func<T> builder) where T : class
+        public T Get<T>(Func<T>? builder = null) where T : class
         {
+            builder ??= GetDefaultConstructorObject<T>;
+
             var srv = _services.Find(t => t is T);
             if (srv != null) return (T)srv;
 
@@ -39,17 +30,17 @@ namespace ServiceManager
             throw new Exception($"No service of type {typeof(T).Name} was found");
         }
 
-        public T Add<T>() where T : class
+        /// <summary>
+        /// Adds an instance of the given type to the service manager
+        /// </summary>
+        /// <typeparam name="T">given class type</typeparam>
+        /// <param name="builder">optional function for building the instance</param>
+        /// <returns>the added instance</returns>
+        public T Add<T>(Func<T>? builder = null) where T : class
         {
-            return Add<T>(GetDefaultConstructorObject<T>);
-        }
-
-        public T Add<T>(Func<T> builderOverride) where T : class
-        {
-            var srv = _services.Find(t => t is T);
-            if (srv != null) return (T)srv;
-
-            srv = builderOverride.Invoke();
+            builder ??= GetDefaultConstructorObject<T>;
+            
+            var srv = builder.Invoke();
             _services.Add(srv);
             return (T)srv;
         }
@@ -61,7 +52,7 @@ namespace ServiceManager
         /// <returns>True if a service instance of type T exists. False otherwise</returns>
         public bool Has<T>() => _services.Exists(srv => srv is T);
 
-        private T GetDefaultConstructorObject<T>()
+        private static T GetDefaultConstructorObject<T>()
         {
             var type = typeof(T);
             var ctor = type.GetConstructor(new Type[] { });
